5b2c4da105a2f0311cfd5ceb43469064de13c3f2
PerrySharePoint_cp <SND\PerrySharePoint_cp>
2013-02-08 16:20:20 -0800

    Add check in Program.cs that SPFarm.Local exists (backported from SP2010 version). Add spver class to encapsulate SharePoint version string.

--- FeatureAdmin MOSS VS 2008/Program.cs
+++ FeatureAdmin MOSS VS 2008/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint;
 
 // Codeplex Project http://FeatureAdmin.codeplex.com
 // Started by Achim Ismaili in September 2009 - email: achim@ismaili.de
@@ -16,6 +18,19 @@ namespace FeatureAdmin
         [STAThread]
         static void Main()
         {
+
+            if (SPFarm.Local == null)
+            {
+                string msg = "Cannot find local SharePoint Farm. "
+                    + "Either this account has not enough access to the SharePoint config db"
+                    + " (dbReader is not sufficient, dbOwner is recommended)"
+                    + " or SharePoint " + spver.SharePointVersion
+                    + " is not installed on this machine. "
+                    + " FeatureAdmin will close now.";
+                MessageBox.Show(msg);
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());
