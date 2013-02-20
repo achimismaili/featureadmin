0efcf434fb021366aaea9586a3c918e37f8a4be1
PerrySharePoint_cp <SND\PerrySharePoint_cp>
2013-02-19 19:27:34 -0800

    FeatureAdmin2010: Import spver class from FeatureAdmin2007, and import simplified Program.cs from FeatureAdmin2007

--- FeatureAdmin2010/Program.cs
+++ FeatureAdmin2010/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Windows.Forms;
 using Microsoft.SharePoint.Administration;
 using Microsoft.SharePoint;
-using System.Security.Principal;
 
 // Codeplex Project http://FeatureAdmin.codeplex.com
 // Started by Achim Ismaili in September 2009 - email: achim@ismaili.de
@@ -15,34 +14,26 @@ namespace FeatureAdmin
 {
     static class Program
     {
-        // run feature admin with elevated privileges
-        public static bool adminMode = false;
-
         /// <summary>The main entry point for the application.</summary>
         [STAThread]
         static void Main()
         {
             
-           
-            if (SPFarm.Local == null )
+            if (SPFarm.Local == null)
             {
-
-                //WindowsIdentity identity = WindowsIdentity.GetCurrent();
-                //WindowsPrincipal principal = new WindowsPrincipal(identity);
-                //bool isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
-
-            
-                    MessageBox.Show("Cannot find farm. Either this account has not enough access to the SharePoint config db (dbReader is not sufficient, dbOwner is recommended)" +
-                        " or SharePoint 2010 is not installed on this machine. FeatureAdmin will close now.");
+                string msg = "Cannot find local SharePoint Farm. "
+                    + "Either this account has not enough access to the SharePoint config db"
+                    + " (dbReader is not sufficient, dbOwner is recommended)"
+                    + " or SharePoint " + spver.SharePointVersion
+                    + " is not installed on this machine. "
+                    + " FeatureAdmin will close now.";
+                MessageBox.Show(msg);
                     return;
                 }
-
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());
         }
-
-
     }
 }
