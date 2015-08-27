9bb0d0b059449ea9d16f59c4d83cd2d973bdf643
PerrySharePoint_cp <SND\PerrySharePoint_cp>
2015-08-27 10:26:45 -0700

    FeatureAdmin2007-VS2010: Fix faulty feature finder to continue, and allow skipping

--- FeatureAdmin2007-VisualStudio2010/FrmMain.cs
+++ FeatureAdmin2007-VisualStudio2010/FrmMain.cs
@@ -1093,13 +1093,17 @@ namespace FeatureAdmin
                             + "Found in " + location + "." + Environment.NewLine
                             + " Should it be removed from the farm?";
                         logDateMsg(msgString);
-                        if (MessageBox.Show(msgString, "Success! Please Decide",
-                            MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                        string caption = "Found Faulty Feature";
+                        DialogResult response = MessageBox.Show(msgString, caption, 
+                            MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                        if (response == DialogResult.Yes)
                         {
                             removeFeaturesWithinFarm(feature.DefinitionId, scope);
                         }
-
-                        return true;
+                        if (response == DialogResult.Cancel)
+                        {
+                            return true;
+                        }
                     }
                 }
             }
