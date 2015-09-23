ca8038c00540eef2407f470e44a37a311fb8494a
PerrySharePoint_cp <SND\PerrySharePoint_cp>
2015-09-22 21:02:23 -0700

    Unify spver.cs across all versions via compiler conditionals

--- FeatureAdmin2007-VisualStudio2008-deprecated/spver.cs
+++ FeatureAdmin2007-VisualStudio2008-deprecated/spver.cs
@@ -1,11 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FeatureAdmin
 {
     class spver
     {
-        public static string SharePointVersion = "2007";
+        #if (SP2007)
+            public static string SharePointVersion = "2007";
+        #endif
+        #if (SP2010)
+            public static string SharePointVersion = "2010";
+        #endif
+        #if (SP2013)
+            public static string SharePointVersion = "2013";
+        #endif
     }
 }
