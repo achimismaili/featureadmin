87fe09fccffd81bb9e1f9652c0f8b3d2754a91da
p <p@w.com>
2016-03-03 11:57:01 -0500

    FeatureAdmin2013: Feature: Copy from FeatureAdmin2010 (from github commit 0a47c5e0a8d5f0910c9eb09f9ec62020060ca84f)

--- FeatureAdmin2013-VisualStudio2012/Feature.cs
+++ FeatureAdmin2013-VisualStudio2012/Feature.cs
@@ -1,84 +1,95 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel;
 using Microsoft.SharePoint;
-
-
-using System.Windows.Forms;
 
 
 namespace FeatureAdmin
 {
-    public class Feature : IComparable
+    public class Feature : IComparable, INotifyPropertyChanged
     {
+        public const int COMPATINAPPLICABLE = -8;
+        public const int COMPATUNKNOWN = -6;
+
         #region class variables
-
-        Guid _id = Guid.Empty;
-        public Guid Id
-        {
-            get { return _id; }
-        }
-
-        String _name = string.Empty;
-        public String Name
-        {
-            get { return _name; }
-            set { _name = value; }
-        }
-
-        int _compatibilityLevel;
-        public int CompatibilityLevel
-        {
-            get { return _compatibilityLevel; }
-            set { _compatibilityLevel = value; }
-        }
-
-        SPFeatureScope _scope = SPFeatureScope.ScopeInvalid;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Guid Id { get; private set; }
+        private SPFeatureScope _Scope = SPFeatureScope.ScopeInvalid;
         public SPFeatureScope Scope
         {
-            get { return _scope; }
-            set { _scope = value; }
+            get { return _Scope; }
+            set
+            {
+                _Scope = value;
+                ScopeAbbrev = ScopeAbbrevConverter.ScopeToAbbrev(value);
+            }
         }
-
-        String _exceptionMsg = "";
-        public String ExceptionMsg
+        public string ScopeAbbrev { get; private set; } // more legible scope names -- see ScopeAbbrevConverter
+        public int CompatibilityLevel { get; set; }
+        public string Name { get; set; }
+        public bool IsFaulty { get; set; }
+        public string Faulty { get { return (IsFaulty ? "Faulty" : ""); } }
+        public string ExceptionMsg { get; set; }
+        private int? _activations = 0;
+        public int? Activations
         {
-            get { return _exceptionMsg; }
-            set { _exceptionMsg = value; }
+            get
+            {
+                return _activations;
+            }
+            set
+            {
+                _activations = value;
+                OnPropertyChanged("Activations");
+            }
         }
-
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        #endregion
 
         public Feature(Guid id)
         {
-            this._id = id;
+            this.Id = id;
+            this.Scope = SPFeatureScope.ScopeInvalid;
+            this.CompatibilityLevel = COMPATUNKNOWN;
         }
 
         public Feature(Guid id, SPFeatureScope scope)
         {
-            this._id = id;
-            this._scope = scope;
+            this.Id = id;
+            this.Scope = scope;
+            this.CompatibilityLevel = COMPATUNKNOWN;
         }
 
-#endregion
 
         /// <summary>overwrite method for ToString - defines, what is shown of a Feature Class as string</summary>
         /// <returns>Feature Information string with scope, name and Guid</returns>
         public override string ToString()
         {
-            String result = string.Empty;
-
-            string idstr = String.Format("{1}/{0}", this._id, this._compatibilityLevel);
-            if (this._compatibilityLevel == FeatureManager.COMPATINAPPLICABLE)
+            string idstr = "";
+            // ID (with compatibility level if available for this SharePoint version)
+            if (this.CompatibilityLevel == COMPATINAPPLICABLE)
             {
-                idstr = String.Format("{0}", this._id);
-            }
-            if (String.IsNullOrEmpty(_name))
-            {
-                result = String.Format("ERROR READING FEATURE [{0}], Scope: {1}", idstr, this._scope.ToString());
+                idstr = String.Format("{0}", this.Id);
             }
             else
             {
-                result = String.Format("{2}: '{1}' [{0}]", idstr, this._name, this._scope.ToString());
+                idstr = String.Format("{1}/{0}", this.Id, this.CompatibilityLevel);
+            }
+            // Combine name & Id
+            String result = string.Empty;
+            if (String.IsNullOrEmpty(this.Name))
+            {
+                result = String.Format("ERROR READING FEATURE [{0}], Scope: {1}", idstr, this.Scope.ToString());
+            }
+            else
+            {
+                result = String.Format("{2}: '{1}' [{0}]", idstr, this.Name, this.Scope.ToString());
             }
             return result;
         }
@@ -88,56 +99,41 @@ namespace FeatureAdmin
         {
             if (ExceptionMsg != "") ExceptionMsg += "; ";
             ExceptionMsg += ExceptionSerializer.ToString(exc);
+            IsFaulty = true;
         }
 
-        // sort the features: first Farm, Web App, Site then Web, after this, alphabetically after the name
+        // sort the features: 
+        // First scope (Farm, Web App, Site then Web)
+        // Then name alphabetically
+        // Then Id
+        // Then CompatibilityLevel
+        // Note: Need to have a fixed stable sort, so compare everything including Ids & CompatLevel
         public int CompareTo(object obj)
         {
-            if (obj is Feature)
+            if (!(obj is Feature))
             {
-                int iVal = 0;
-                int oVal = 0;
-
-                switch (this.Scope)
-                {
-                    case SPFeatureScope.Farm:
-                        iVal += 100; break;
-                    case SPFeatureScope.WebApplication:
-                        iVal += 200; break;
-                    case SPFeatureScope.Site:
-                        iVal += 300; break;
-                    case SPFeatureScope.Web:
-                        iVal += 400; break;
-                    default:
-                        iVal += 500; break;
-                }
-
-                switch (((Feature)obj).Scope)
-                {
-                    case SPFeatureScope.Farm:
-                        oVal += 100; break;
-                    case SPFeatureScope.WebApplication:
-                        oVal += 200; break;
-                    case SPFeatureScope.Site:
-                        oVal += 300; break;
-                    case SPFeatureScope.Web:
-                        oVal += 400; break;
-                    default:
-                        oVal += 500; break;
-
-                }
-
-                if (this.Name != null)
-                {
-                    iVal += this.Name.CompareTo(((Feature)obj).Name);
-                }
-
-                // if iVal is higher than oVal in value, it will be far down in the list ...
-                return (iVal - oVal);
-            }
-            else
                 throw (new System.ArgumentException("Object is not a Feature like the instance"));
+            }
+            Feature other = (Feature)obj;
 
+            int cmp = this.Scope.CompareTo(other.Scope);
+            if (cmp != 0)
+            {
+                return cmp;
+            }
+
+            cmp = string.Compare(this.Name, other.Name);
+            if (cmp != 0)
+            {
+                return cmp;
+            }
+            cmp = this.Id.CompareTo(other.Id);
+            if (cmp != 0)
+            {
+                return cmp;
+            }
+            cmp = this.CompatibilityLevel.CompareTo(other.CompatibilityLevel);
+            return cmp;
         }
     }
 }
