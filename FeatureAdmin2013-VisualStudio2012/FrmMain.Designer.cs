532b557420a722dec0539e40b32dc6a46fe36789
p <p@w.com>
2016-03-04 19:33:36 -0500

    FeatureAdmin2013: Remove unused hardcoded version title from FrmMain.Designer.cs

--- FeatureAdmin2013-VisualStudio2012/FrmMain.Designer.cs
+++ FeatureAdmin2013-VisualStudio2012/FrmMain.Designer.cs
@@ -703,7 +703,7 @@ namespace FeatureAdmin
             this.ClientSize = new System.Drawing.Size(892, 730);
             this.Controls.Add(this.splitContainerCompleteMainframe);
             this.Name = "FrmMain";
-            this.Text = "FeatureAdmin for SharePoint 2013 - v2.3";
+            this.Text = "FeatureAdmin for SharePoint";
             this.Load += new System.EventHandler(this.FrmMain_Load);
             this.tabControl1.ResumeLayout(false);
             this.FarmFeatures.ResumeLayout(false);
