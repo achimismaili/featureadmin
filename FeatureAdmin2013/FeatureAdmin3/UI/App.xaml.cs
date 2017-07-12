9182af9a6eacd9ace327f984b68f6ab78f52c1c0
Achim Ismaili <achim@ismaili.de>
2017-07-12 21:07:31 +0200

    Moved mainwindow.xaml to subfolder UI

--- FeatureAdmin2013/FeatureAdmin3/UI/App.xaml.cs
+++ FeatureAdmin2013/FeatureAdmin3/UI/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace FeatureAdmin3
+namespace FeatureAdmin3.UI
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- FeatureAdmin2013/FeatureAdmin3/UI/MainWindow.xaml.cs
+++ FeatureAdmin2013/FeatureAdmin3/UI/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FeatureAdmin3
+namespace FeatureAdmin3.UI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
