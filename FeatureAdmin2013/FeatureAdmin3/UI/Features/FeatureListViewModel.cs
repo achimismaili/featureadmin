bf52b5043a07f470ce42002c84e3cb8d406c986f
Achim Ismaili <achim@ismaili.de>
2017-07-13 16:15:29 +0200

    added adorned control from https://www.codeproject.com/Articles/57984/WPF-Loading-Wait-Adorner

--- FeatureAdmin2013/FeatureAdmin3/UI/Features/FeatureListViewModel.cs
+++ FeatureAdmin2013/FeatureAdmin3/UI/Features/FeatureListViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using FeatureAdmin.Models;
+using FeatureAdmin3.Repository;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,28 @@ namespace FeatureAdmin3.UI.Features
 {
     class FeatureListViewModel : BindableBase
     {
+        private IFeatureRepository repo;
+
+        public FeatureListViewModel()
+            :this(new FeatureRepository())
+        { }
+
+        public FeatureListViewModel(IFeatureRepository featureRepository)
+        {
+            repo = featureRepository;
+        }
+
+        private ObservableCollection<FeatureDefinition> featureDefinitions;
+        public ObservableCollection<FeatureDefinition> FeatureDefinitions
+        {
+            get { return featureDefinitions; }
+            set { SetProperty(ref featureDefinitions, value); }
+        }
+
+        public void LoadFeatures()
+        {
+            featureDefinitions = new ObservableCollection<FeatureDefinition>(
+                repo.GetFeatureDefinitions());
+        }
     }
 }
--- FeatureAdmin2013/FeatureAdmin3/UI/Parents/ParentsListViewModel.cs
+++ FeatureAdmin2013/FeatureAdmin3/UI/Parents/ParentsListViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using FeatureAdmin.Models;
+using FeatureAdmin3.Repository;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,28 @@ namespace FeatureAdmin3.UI.Parents
 {
     class ParentsListViewModel : BindableBase
     {
+        private IFeatureRepository repo;
+
+        public ParentsListViewModel()
+            :this(new FeatureRepository())
+        {}
+
+        public ParentsListViewModel(IFeatureRepository featureRepository)
+        {
+            repo = featureRepository;
+        }
+
+        private ObservableCollection<FeatureParent> parents;
+        public ObservableCollection<FeatureParent> Parents
+        {
+            get { return parents; }
+            set { SetProperty(ref parents, value); }
+        }
+
+        public void LoadParents()
+        {
+            parents = new ObservableCollection<FeatureParent>(
+                repo.GetParents());
+        }
     }
 }
