0c4cb069ae42205879f7b027bc051f811a568e21
Achim Ismaili <achim.ismaili@cognizant.com>
2017-10-13 16:01:25 -0700

    RadioButton Scope Fitering working

--- src/FeatureAdmin/ViewModels/LocationListViewModel.cs
+++ src/FeatureAdmin/ViewModels/LocationListViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System;
+using FeatureAdmin.Core.Models.Enums;
 
 namespace FeatureAdmin.ViewModels
 {
@@ -19,11 +20,11 @@ namespace FeatureAdmin.ViewModels
         public LocationListViewModel(IEventAggregator eventAggregator)
         {
 
-                allLocations = new ObservableCollection<Location>();
-                Locations = allLocations;
-                this.eventAggregator = eventAggregator;
-                this.eventAggregator.Subscribe(this);
-            }
+            allLocations = new ObservableCollection<Location>();
+            Locations = allLocations;
+            this.eventAggregator = eventAggregator;
+            this.eventAggregator.Subscribe(this);
+        }
 
         public void Handle(LocationUpdated message)
         {
@@ -42,32 +43,126 @@ namespace FeatureAdmin.ViewModels
 
             allLocations.Add(locationToAdd);
         }
+        private bool scopeFilterAll;
 
-        private string _SearchInput;
-        public string SearchInput
+        private bool scopeFilterFarm;
+        private bool scopeFilterWebApp;
+        private bool scopeFilterSite;
+        private bool scopeFilterWeb;
+        public bool ScopeFilterAll { get {
+                return scopeFilterAll;
+            } set
+            {
+                scopeFilterAll = value;
+                if (scopeFilterAll)
+                {
+                    scopeFilter = null;
+                    FilterResults();
+                }
+            } }
+        public bool ScopeFilterFarm 
         {
-            get { return _SearchInput; }
+            get
+            {
+                return scopeFilterFarm;
+            }
             set
             {
-                _SearchInput = value;
-                FilterResults(_SearchInput);
+                scopeFilterFarm = value;
+                if (scopeFilterFarm)
+                {
+                    scopeFilter = Scope.Farm;
+                    FilterResults();
+                }
+            }
+        }
+        public bool ScopeFilterWebApp
+        {
+            get
+            {
+                return scopeFilterWebApp;
+            }
+            set
+            {
+                scopeFilterWebApp = value;
+                if (scopeFilterWebApp)
+                {
+                    scopeFilter = Scope.WebApplication;
+                    FilterResults();
+                }
+            }
+        }
+        public bool ScopeFilterSite
+        {
+            get
+            {
+                return scopeFilterSite;
+            }
+            set
+            {
+                scopeFilterSite = value;
+                if (scopeFilterSite)
+                {
+                    scopeFilter = Scope.Site;
+                    FilterResults();
+                }
+            }
+        }
+        public bool ScopeFilterWeb
+        {
+            get
+            {
+                return scopeFilterWeb;
+            }
+            set
+            {
+                scopeFilterWeb = value;
+                if (scopeFilterWeb)
+                {
+                    scopeFilter = Scope.Web;
+                    FilterResults();
+                }
             }
         }
 
-        protected void FilterResults(string searchInput)
+        private Scope? scopeFilter;
+
+        private string searchInput;
+        public string SearchInput
         {
-            if (string.IsNullOrWhiteSpace(searchInput))
+            get { return searchInput; }
+            set
             {
-                Locations = new ObservableCollection<Location>(allLocations);
-                return;
+                searchInput = value;
+                FilterResults();
+            }
+        }
+
+        protected void FilterResults()
+        {
+            IEnumerable<Location> searchResult;
+            if (scopeFilter == null)
+            {
+                searchResult = allLocations; ;
+            }
+            else
+            {
+                searchResult = 
+                    allLocations.Where(l => l.Scope == scopeFilter.Value);
+            }
+
+            if (string.IsNullOrWhiteSpace(searchInput) )
+            {
+                Locations = new ObservableCollection<Location>(searchResult);
             }
             else
             {
                 var lowerCaseSearchInput = searchInput.ToLower();
                 Locations = new ObservableCollection<Location>(
-                    allLocations.Where(l => l.DisplayName.ToLower().Contains(lowerCaseSearchInput) ||
-                    l.Url.ToLower().Contains(lowerCaseSearchInput) ));
+                    searchResult.Where(l => l.DisplayName.ToLower().Contains(lowerCaseSearchInput) ||
+                    l.Url.ToLower().Contains(lowerCaseSearchInput)));
             }
+            return;
         }
 
         public void ClearSearchCommand()
