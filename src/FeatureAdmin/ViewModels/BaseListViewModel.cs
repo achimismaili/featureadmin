e2e2bbf5d2aabff6af19a32f0c3fab1d7e6abb67
Achim Ismaili <achim.ismaili@cognizant.com>
2017-10-25 23:59:17 +0200

    Search Filter fixed for LocationViewModel

--- src/FeatureAdmin/ViewModels/BaseListViewModel.cs
+++ src/FeatureAdmin/ViewModels/BaseListViewModel.cs
@@ -14,17 +14,72 @@ namespace FeatureAdmin.ViewModels
 {
     public abstract class BaseListViewModel<T> : Conductor<T>.Collection.OneActive, IHandle<ItemUpdated<T>>, IHandle<SetSearchFilter<T>> where T : BaseItem
     {
-        // The filtered Items
-        public ObservableCollection<T> SearchResultItems { get; private set; }
-
         private IEventAggregator eventAggregator;
+
+        private string searchInput;
+
+        private Scope? selectedScopeFilter;
 
         public BaseListViewModel(IEventAggregator eventAggregator)
         {
             this.eventAggregator = eventAggregator;
             this.eventAggregator.Subscribe(this);
             ScopeFilters = new ObservableCollection<Scope>(Common.Constants.Search.ScopeFilterList);
-            SearchResultItems = new ObservableCollection<T>();
+            allItems = new ObservableCollection<T>();
+        }
+
+        public ObservableCollection<Scope> ScopeFilters { get; private set; }
+
+        public string SearchInput
+        {
+            get { return searchInput; }
+            set
+            {
+                searchInput = value;
+                FilterResults();
+            }
+        }
+
+        // The filtered Items
+        protected ObservableCollection<T> allItems { get; private set; }
+        public Scope? SelectedScopeFilter
+        {
+            get { return selectedScopeFilter; }
+            set
+            {
+                selectedScopeFilter = value;
+                FilterResults();
+            }
+        }
+
+        public void CopyToClipBoard(string textToCopy)
+        {
+            if (!string.IsNullOrEmpty(textToCopy))
+            {
+                Clipboard.SetText(textToCopy);
+            }
+        }
+
+        public void FilterFeature()
+        {
+            var searchFilter = new SetSearchFilter<FeatureDefinition>(
+
+                ActiveItem == null ? string.Empty : ActiveItem.Id.ToString(), null);
+            eventAggregator.BeginPublishOnUIThread(searchFilter);
+        }
+
+        public void FilterLocation()
+        {
+            var searchFilter = new SetSearchFilter<Location>(
+                ActiveItem == null ? string.Empty : ActiveItem.Id.ToString(), null);
+            eventAggregator.BeginPublishOnUIThread(searchFilter);
+        }
+
+        public void FilterThis()
+        {
+            var searchFilter = new SetSearchFilter<T>(
+                ActiveItem == null ? string.Empty : ActiveItem.Id.ToString(), null);
+            Handle(searchFilter);
         }
 
         public void Handle(ItemUpdated<T> message)
@@ -36,85 +91,15 @@ namespace FeatureAdmin.ViewModels
             }
 
             var itemToAdd = message.Item;
-            if (Items.Any(l => l.Id == itemToAdd.Id))
+            if (allItems.Any(l => l.Id == itemToAdd.Id))
             {
-                var existingLocation = Items.FirstOrDefault(l => l.Id == itemToAdd.Id);
-                Items.Remove(existingLocation);
+                var existingLocation = allItems.FirstOrDefault(l => l.Id == itemToAdd.Id);
+                allItems.Remove(existingLocation);
             }
 
-            Items.Add(itemToAdd);
+            allItems.Add(itemToAdd);
             FilterResults();
         }
-
-        public ObservableCollection<Scope> ScopeFilters { get; private set; }
-
-        private Scope? selectedScopeFilter;
-        public Scope? SelectedScopeFilter
-        {
-            get { return selectedScopeFilter; }
-            set
-            {
-                selectedScopeFilter = value;
-                FilterResults();
-            }
-        }
-
-        private string searchInput;
-        public string SearchInput
-        {
-            get { return searchInput; }
-            set
-            {
-                searchInput = value;
-                FilterResults();
-            }
-        }
-
-        // Searching for results can be different in derived types
-        protected abstract Func<T, bool> GetSearchForGuid(Guid guid);
-
-
-        // Searching for results can be different in derived types
-        protected abstract Func<T, bool> GetSearchForString(string searchString);
-
-
-        protected void FilterResults()
-        {
-            IEnumerable<T> searchResult;
-
-            if (string.IsNullOrEmpty(searchInput))
-            {
-                searchResult = Items;
-            }
-            else
-            {
-                Guid idGuid;
-                Guid.TryParse(searchInput, out idGuid);
-
-                // if searchInput is not a guid, seachstring will always be a guid.empty
-                // to also catch, if user intentionally wants to search for guid empty, this is checked here, too
-                if (searchInput.Equals(Guid.Empty.ToString()) || idGuid != Guid.Empty)
-                {
-                    searchResult = Items.Where(GetSearchForGuid(idGuid));
-                }
-                else
-                {
-                    var lowerCaseSearchInput = searchInput.ToLower();
-                    searchResult =
-                        Items.Where(GetSearchForString(lowerCaseSearchInput));
-                }
-
-            }
-
-            if (SelectedScopeFilter != null)
-            {
-                searchResult =
-                    searchResult.Where(l => l.Scope == SelectedScopeFilter.Value);
-            }
-
-            SearchResultItems = new ObservableCollection<T>(searchResult);
-        }
-
         public void Handle(SetSearchFilter<T> message)
         {
             if (message == null)
@@ -134,34 +119,49 @@ namespace FeatureAdmin.ViewModels
 
         }
 
-        public void FilterThis()
+        protected void FilterResults()
         {
-            var searchFilter = new SetSearchFilter<T>(
-                ActiveItem == null ? string.Empty : ActiveItem.Id.ToString(), null);
-            Handle(searchFilter);
-        }
+            IEnumerable<T> searchResult;
 
-        public void FilterLocation()
-        {
-            var searchFilter = new SetSearchFilter<Location>(
-                ActiveItem == null ? string.Empty : ActiveItem.Id.ToString(), null);
-            eventAggregator.BeginPublishOnUIThread(searchFilter);
-        }
-
-        public void FilterFeature()
-        {
-            var searchFilter = new SetSearchFilter<FeatureDefinition>(
-
-                ActiveItem == null ? string.Empty : ActiveItem.Id.ToString(), null);
-            eventAggregator.BeginPublishOnUIThread(searchFilter);
-        }
-
-        public void CopyToClipBoard(string textToCopy)
-        {
-            if (!string.IsNullOrEmpty(textToCopy))
+            if (string.IsNullOrEmpty(searchInput))
             {
-                Clipboard.SetText(textToCopy);
+                searchResult = allItems;
             }
+            else
+            {
+                Guid idGuid;
+                Guid.TryParse(searchInput, out idGuid);
+
+                // if searchInput is not a guid, seachstring will always be a guid.empty
+                // to also catch, if user intentionally wants to search for guid empty, this is checked here, too
+                if (searchInput.Equals(Guid.Empty.ToString()) || idGuid != Guid.Empty)
+                {
+                    searchResult = allItems.Where(GetSearchForGuid(idGuid));
+                }
+                else
+                {
+                    var lowerCaseSearchInput = searchInput.ToLower();
+                    searchResult =
+                       allItems.Where(GetSearchForString(lowerCaseSearchInput));
+                }
+
+            }
+
+            if (SelectedScopeFilter != null)
+            {
+                searchResult =
+                    searchResult.Where(l => l.Scope == SelectedScopeFilter.Value);
+            }
+
+            Items.Clear();
+            Items.AddRange(searchResult);
         }
+
+        // Searching for results can be different in derived types
+        protected abstract Func<T, bool> GetSearchForGuid(Guid guid);
+
+
+        // Searching for results can be different in derived types
+        protected abstract Func<T, bool> GetSearchForString(string searchString);
     }
 }
