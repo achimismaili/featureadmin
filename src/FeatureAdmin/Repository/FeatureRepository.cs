cdf08eef37ad4bc0c9d6c9dd9dfd51a84e60eb73
Achim Ismaili <achim.ismaili@cognizant.com>
2018-05-21 17:52:24 +0200

    Set OrigoDb to completely be in memory, so that no directory is created. see https://github.com/DevrexLabs/OrigoDB/issues/24

--- src/FeatureAdmin/Repository/FeatureRepository.cs
+++ src/FeatureAdmin/Repository/FeatureRepository.cs
@@ -25,8 +25,8 @@ namespace FeatureAdmin.Repository
             var config = new EngineConfiguration();
             config.PersistenceMode = PersistenceMode.ManualSnapshots;
             // see https://github.com/DevrexLabs/OrigoDB/issues/24
-            // config.SetCommandStoreFactory(cfg => new OrigoDB.Core.Test.InMemoryCommandStore(cfg));
-            // config.SetSnapshotStoreFactory(cfg => new OrigoDB.Core.Test.InMemorySnapshotStore(cfg));
+            config.SetCommandStoreFactory(cfg => new OrigoDB.Core.Test.InMemoryCommandStore(cfg));
+            config.SetSnapshotStoreFactory(cfg => new OrigoDB.Core.Test.InMemorySnapshotStore(cfg));
             store = Db.For<OrigoDb.FeatureModel>(config);
 
             this.eventAggregator = eventAggregator;
