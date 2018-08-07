91eaa6f2d83c7184cc549ea3845da26e7964316e
Achim Ismaili <achim@ismaili.de>
2018-08-07 17:19:11 +0200

    Set featureadmin version to 3.0.1 Set elevated setting to True by default Started excel file with test cases

--- src/FeatureAdmin/Properties/Settings.Designer.cs
+++ src/FeatureAdmin/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace FeatureAdmin.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool elevatedPrivileges {
             get {
                 return ((bool)(this["elevatedPrivileges"]));
