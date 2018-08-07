f34d1c7a6ca774d665d98c906fa3be473734b7e8
Achim Ismaili <achim@ismaili.de>
2018-08-07 18:03:34 +0200

    Improved Log View - log icons are shown better and have tool tip - log font is adjusted to severity - each log entry has visible border

--- src/FeatureAdmin/Messages/LogMessage.cs
+++ src/FeatureAdmin/Messages/LogMessage.cs
@@ -15,6 +15,33 @@ namespace FeatureAdmin.Messages
         public LogLevel Level { get; }
         public string Text { get; }
 
+        public string FontWeight { get
+            {
+                switch (Level)
+                {
+                    //case LogLevel.Debug:
+                    //    break;
+                    //case LogLevel.Information:
+                    //    break;
+                    case LogLevel.Warning:
+                        return "Normal";
+                    case LogLevel.Error:
+                        return "Bold";
+                    default:
+                        return "Thin";
+                }
+            }
+        }
+
+        public string LogLine { get
+            {
+                return string.Format("{0} - {1}: {2}",
+                   ShortTime,
+                   ShortLevel,
+                   Text);
+            }
+        }
+
         public string ShortTime { get {
                 return Time.ToString("T");
             } }
