4848cbcc6933620ceecfac679dac34ae3824a659
Achim Ismaili <achim@ismaili.de>
2018-09-09 19:47:46 +0200

    Made featureadmin automatically switch between demo mode and sp2013 mode

--- src/FeatureAdmin/AppBootstrapper.cs
+++ src/FeatureAdmin/AppBootstrapper.cs
@@ -2,6 +2,7 @@
 using Caliburn.Metro.Autofac;
 using Caliburn.Micro;
 using FeatureAdmin.ViewModels;
+using System.Reflection;
 
 namespace FeatureAdmin
 {
@@ -16,8 +17,22 @@ namespace FeatureAdmin
 
 #if (SP2013)
             builder.RegisterType<Backends.Sp2013.Services.SpDataService>().As<Core.Services.IDataService>().SingleInstance();
-#else
+#elif (DEMO)
             builder.RegisterType<Backends.Demo.Services.DemoDataService>().As<Core.Services.IDataService>().SingleInstance();
+#else
+            string pathSp2013 = @"C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.dll";
+
+            if (System.IO.File.Exists(pathSp2013))
+            {
+                System.Console.WriteLine("SharePoint 2013 assembly found");
+                builder.RegisterType<Backends.Sp2013.Services.SpDataService>().As<Core.Services.IDataService>().SingleInstance();
+            }
+            else
+            {
+                System.Console.WriteLine("No SharePoint installation found, starting demo mode ...");
+                builder.RegisterType<Backends.Demo.Services.DemoDataService>().As<Core.Services.IDataService>().SingleInstance();
+            }
+
 #endif
 
             var assembly = typeof(AppViewModel).Assembly;
